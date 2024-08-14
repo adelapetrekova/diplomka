9222f82cdbb170f37b17840dda67b2d7115dbca0
adelapetrekova <adela.petrekova01@upol.cz>
2024-08-14 21:23:30 +0200

    netusim random comit

---
+++ barvy.py
@@ -0,0 +1,6 @@
+from termcolor import colored
+
+print(colored("This is red text", "red"))
+print(colored("This is green text", "green"))
+print(colored("This is yellow text", "yellow"))
+print("This is default text")
